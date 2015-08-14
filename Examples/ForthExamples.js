c3a13e0d1754d6a2004d6feadce276487858479f
karudedios <c.dedios@outlook.com>
2015-08-14 17:30:04 -0400

    ProgrammingIsPointless (JS)
    Creating examples for Utils.js and Forth.js

---
+++ Examples/ForthExamples.js
@@ -0,0 +1,24 @@
+import { compose, pipe } from '../Utils/Utils.js';
+import { MATH, STACK, LOGIC, EQUALITY, IO, MISC } from '../Forth/Forth.js';
+
+(() => {
+  let { ADD, SUB, MUL, DIV } = MATH;
+  let { LIT, DUP, DROP, SWAP, ROT, RROT } = STACK;
+  let { IF } = LOGIC;
+  let { GT } = EQUALITY;
+  let { PRINT } = IO;
+  let { NOP } = MISC;
+
+  let ONE = LIT(1);
+
+  let QUADRATIC = compose(DUP, RROT, MUL, ROT, ADD, MUL, ADD);
+  pipe([], compose(LIT(1), LIT(2), LIT(2), LIT(10), QUADRATIC, PRINT));
+
+  let FAC = (l) => pipe(l, compose(DUP, ONE, GT, IF(compose(DUP, ONE, SUB, FAC, MUL), NOP)));
+  pipe([], compose(LIT(7), FAC, PRINT));
+
+  let EULER_GEN = (l) => pipe(l, compose(DUP, FAC, ONE, SWAP, DIV, ROT, ADD, SWAP, ONE, SUB, DUP, LIT(0), GT, IF(EULER_GEN, DROP)));
+  pipe([], compose(LIT(1), LIT(12), EULER_GEN, PRINT));
+
+  return {};
+})();
---
+++ Examples/UtilsExamples.js
@@ -0,0 +1,52 @@
+import { compose, pipe, curry } from '../Utils/Utils.js';
+
+/**
+ * Normal vs Curried
+ */
+
+let sumThree = (a, b, c) => a + b + c;
+let curriedSumThree = curry(sumThree);
+
+let partialSumThree = (a) => (b) => (c) => a + b + c;
+let curriedPartialSumThree = curry(partialSumThree);
+
+console.log(
+  sumThree(1, 2, 3),
+  curriedSumThree(1)(2)(3),
+  partialSumThree(1)(2)(3),
+  curriedPartialSumThree(1, 2, 3)
+);
+
+/**
+ * Curry over and over again
+ */
+
+let doubleCurriedSumThree = curry(curriedSumThree);
+let tripleCurriedSumThree = curry(doubleCurriedSumThree);
+
+console.log(
+  doubleCurriedSumThree(1, 2, 3),
+  tripleCurriedSumThree(1)(2)(3)
+);
+
+/**
+ * Order doesn't matter
+ */
+
+let addFourNumbers = curry((a, b) => (c, d) => a + b + c + d);
+
+console.log(
+  addFourNumbers(1, 2, 3, 4),
+  addFourNumbers(1)(2, 3, 4),
+  addFourNumbers(1, 2)(3, 4),
+  addFourNumbers(1, 2, 3)(4),
+  addFourNumbers(1)(2, 3)(4),
+  addFourNumbers(1)(2)(3, 4),
+  addFourNumbers(1, 2)(3)(4),
+  addFourNumbers(1)(2)(3)(4)
+);
+
+let a = function(x, y) {}
+let ca = curry(a);
+
+console.log(curry.length, ca.length)
