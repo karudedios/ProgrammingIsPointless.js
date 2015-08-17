e861e7c4eb36d46a4e11db80d56ba18ec3115aed
karudedios <c.dedios@outlook.com>
2015-08-17 15:16:55 -0400

    Refactoring Utils
    Putting code inside an IIFE.

--- Examples/UtilsExamples.js
+++ Examples/UtilsExamples.js
@@ -1,52 +1,50 @@
 import { compose, pipe, curry } from '../Utils/Utils.js';
 
-/**
- * Normal vs Curried
- */
+(() => {
+  /**
+   * Normal vs Curried
+   */
 
-let sumThree = (a, b, c) => a + b + c;
-let curriedSumThree = curry(sumThree);
+  let sumThree = (a, b, c) => a + b + c;
+  let curriedSumThree = curry(sumThree);
 
-let partialSumThree = (a) => (b) => (c) => a + b + c;
-let curriedPartialSumThree = curry(partialSumThree);
+  let partialSumThree = (a) => (b) => (c) => a + b + c;
+  let curriedPartialSumThree = curry(partialSumThree);
 
-console.log(
-  sumThree(1, 2, 3),
-  curriedSumThree(1)(2)(3),
-  partialSumThree(1)(2)(3),
-  curriedPartialSumThree(1, 2, 3)
-);
+  console.log(
+    sumThree(1, 2, 3),
+    curriedSumThree(1)(2)(3),
+    partialSumThree(1)(2)(3),
+    curriedPartialSumThree(1, 2, 3)
+  );
 
-/**
- * Curry over and over again
- */
+  /**
+   * Curry over and over again
+   */
 
-let doubleCurriedSumThree = curry(curriedSumThree);
-let tripleCurriedSumThree = curry(doubleCurriedSumThree);
+  let doubleCurriedSumThree = curry(curriedSumThree);
+  let tripleCurriedSumThree = curry(doubleCurriedSumThree);
 
-console.log(
-  doubleCurriedSumThree(1, 2, 3),
-  tripleCurriedSumThree(1)(2)(3)
-);
+  console.log(
+    doubleCurriedSumThree(1, 2, 3),
+    tripleCurriedSumThree(1)(2)(3)
+  );
 
-/**
- * Order doesn't matter
- */
+  /**
+   * Order doesn't matter
+   */
 
-let addFourNumbers = curry((a, b) => (c, d) => a + b + c + d);
+  let addFourNumbers = curry((a, b) => (c, d) => a + b + c + d);
 
-console.log(
-  addFourNumbers(1, 2, 3, 4),
-  addFourNumbers(1)(2, 3, 4),
-  addFourNumbers(1, 2)(3, 4),
-  addFourNumbers(1, 2, 3)(4),
-  addFourNumbers(1)(2, 3)(4),
-  addFourNumbers(1)(2)(3, 4),
-  addFourNumbers(1, 2)(3)(4),
-  addFourNumbers(1)(2)(3)(4)
-);
+  console.log(
+    addFourNumbers(1, 2, 3, 4),
+    addFourNumbers(1)(2, 3, 4),
+    addFourNumbers(1, 2)(3, 4),
+    addFourNumbers(1, 2, 3)(4),
+    addFourNumbers(1)(2, 3)(4),
+    addFourNumbers(1)(2)(3, 4),
+    addFourNumbers(1, 2)(3)(4),
+    addFourNumbers(1)(2)(3)(4)
+  );
 
-let a = function(x, y) {}
-let ca = curry(a);
-
-console.log(curry.length, ca.length)
+})();
